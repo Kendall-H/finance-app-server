0f4cba3348bde51b5bb08565073e480505c36d17
Kendall Haskell <kendallr13@hotmail.com>
2022-03-16 14:23:59 -0600

    add routes for login and auth

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -10,24 +13,36 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "gfdgdfgfdgfdgfdgfd.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/financeDB");
-mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: String,
-    password: String
+    password: String,
+    cryptos: [cryptoSchema],
+    stocks: [stockSchema]
 });
 
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ['password']
-});
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const cryptoSchema = {
     name: String,
@@ -43,41 +58,94 @@ const stockSchema = {
     userAmount: Number
 }
 
+//check if a user is authenticated
+app.get("/financials", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("financials");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+
+//register a new user
 app.post("/register", function (req, res) {
 
-    const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.enteredEmail,
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            //redirect to register page with error
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                //redirect to financials in page
+            })
+        }
+    })
+});
+
+//log a user in and direct them to the financials page
+app.post("/login", function (res, req) {
+    const user = new User({
+        username: req.body.username,
         password: req.body.password
     });
-    newUser.save(function (err) {
+
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            //render login page
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/financials");
+            })
         }
     })
-})
+});
 
-app.post("/login", function (res, req) {
-    const username = req.body.email;
-    const password = req.body.password;
+app.post("/cryptos", function (req, res) {
+    const submittedCrypto = req.body.crypto;
+    // Check if crypto is valid or if it isnt already in user's crypto list
 
-    User.findOne({
-        email: username
-    }, function (err, foundUser) {
+    User.findById(req.user.id, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    // log the user in
-                }
+                foundUser.cryptos.push(submittedCrypto);
+                foundUser.Save(function () {
+                    // might need to change these redirects
+                    res.redirect("/financials");
+                });
             }
         }
-    })
+    });
 });
+
+app.post("/stocks", function (req, res) {
+    const submittedStock = req.body.stock;
+    // Check if stock is valid or if it isnt already in user's stock list
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.stocks.push(submittedStock)
+                foundUser.Save(function () {
+                    // might need to change these redirects
+                    res.redirect("/financials");
+                });
+            }
+        }
+    });
+});
+
+//logout a user
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("finance server running on port 3000")
